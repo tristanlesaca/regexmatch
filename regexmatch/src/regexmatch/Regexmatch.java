30d0c7190b5b7007f2fd53d739e72705b4c691cd
Tristan Lesaca <tristanlesaca@gmail.com>
2016-09-15 08:24:40 +0800

    adds second regex

--- regexmatch/src/regexmatch/Regexmatch.java
+++ regexmatch/src/regexmatch/Regexmatch.java
@@ -6,8 +6,13 @@ public class Regexmatch {
         return str.matches("[tT]rue");
     }
     
+    public static boolean containsFalse(String str){
+        return str.matches(".*false.*");
+    }
+    
     public static void main(String[] args) {
         System.out.println(isTrue("True"));
+        System.out.println(containsFalse("false"));
     }
     
 }
