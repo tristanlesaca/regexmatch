1f0fd0f398974dc6b92bb5a8a986f214d0f7741b
Tristan Lesaca <tristanlesaca@gmail.com>
2016-09-15 08:17:03 +0800

    adds first regex

--- regexmatch/src/regexmatch/Regexmatch.java
+++ regexmatch/src/regexmatch/Regexmatch.java
@@ -2,8 +2,12 @@ package regexmatch;
 
 public class Regexmatch {
     
+    public static boolean isTrue(String str){
+        return str.matches("[tT]rue");
+    }
+    
     public static void main(String[] args) {
-        
+        System.out.println(isTrue("True"));
     }
     
 }
